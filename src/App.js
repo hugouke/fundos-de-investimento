fb03a6a1a565ce76ff4d24dc5da875c3111ce696
Hugo Nogueira <nogueiradeoliveira@gmail.com>
2020-02-03 16:59:09 -0300

    Big-bang!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./css/App.css";
+import { Button, Colors, Link, Grid, Cell } from "react-foundation";
+import headerBackground from "./img/background.jpg";
 
 function App() {
+  var header = {
+    backgroundImage: `url(${headerBackground})`
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header" style={header}>
+        <h1>Lista de Fundos de Investimento</h1>
+        <p>Conheça a nossa lista de fundos</p>
+      </div>
+      <div className="grid-container">
+        <Grid className="display">
+          <Cell small={12} medium={10}>
+            <div className="card">
+              <input placeholder="Busca fundo por nome" />
+            </div>
+          </Cell>
+          <Cell small={12} medium={2}>
+            <div className="card">
+              <input type="checkbox" className="checkboxAzul" />
+              RENDA FIXA
+            </div>
+          </Cell>
+        </Grid>
+      </div>
     </div>
   );
 }
